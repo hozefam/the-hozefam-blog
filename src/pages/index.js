696d97519ec82e7bdf849c55039bf1392a1b8b0a
Hozefa <hozefam@gmail.com>
2020-04-25 21:55:47 +0300

    index page list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import { Layout } from '../components/Layout';
+import { graphql } from 'gatsby';
 
-export default () => {
+export default ({ data }) => {
   return (
     <>
-      <Layout />
+      <Layout>
+        {data.allMdx.nodes.map(({ excerpt, frontmatter }) => {
+          return (
+            <>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </>
+          );
+        })}
+      </Layout>
     </>
   );
 };
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 10)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
