046f221b13dbdd279287eba5ff117a80d275d4a2
Hozefa <hozefam@gmail.com>
2020-04-25 20:47:27 +0300

    add posts

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    { resolve: `gatsby-plugin-mdx`, options: { extensions: [`.mdx`, `.md`] } },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: { path: `${__dirname}/posts`, name: `posts` },
+    },
+  ],
+};
