974200a674aafd660fe8fb5165a9e2c9d35d8908
Hozefa <hozefam@gmail.com>
2020-04-26 00:20:22 +0300

    add previous and next blog links

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { Layout } from '../components/Layout';
 
-export default ({ data }) => {
+export default ({ data, pageContext }) => {
   const { frontmatter, body } = data.mdx;
+  const { previous, next } = pageContext;
   return (
     <Layout>
       <h1>{frontmatter.title}</h1>
       <p>{frontmatter.date}</p>
       <MDXRenderer>{body}</MDXRenderer>
+      {previous === false ? null : (
+        <>
+          {previous && (
+            <Link to={previous.fields.slug}>{previous.frontmatter.title}</Link>
+          )}
+        </>
+      )}
+      {next === false ? null : (
+        <>
+          {next && <Link to={next.fields.slug}>{next.frontmatter.title}</Link>}
+        </>
+      )}
     </Layout>
   );
 };
